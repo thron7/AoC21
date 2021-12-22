ecbd88f72ab8b313c04af59950d62eb864741f58
thron7 <thron7@users.sourceforge.net>
2021-12-22 17:52:15 +0100

    solve_2 und abgabe

--- day4/day4.pl
+++ day4/day4.pl
@@ -2,16 +2,75 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 :- use_module(library(apply)).
+:- op(100,fy,~).
 
 main(Solution1, Solution2):-
     main('input.txt',Solution1, Solution2).
 main(File, Solution1, Solution2):-
-    read_data(File,[L1|Lines]), !,
+    read_data(File,[L1|Lines]),
     number_calls(L1,Calls),
     boards(Lines,Boards),
-    prepare(Lines, BitLines),
-    solve_1(BitLines,Solution1),
-    solve_2(BitLines,Solution2).
+    solve_1(Calls,Boards,Solution1),
+    solve_2(Calls,Boards,Solution2).
+
+solve_1(Calls,Boards,R):-
+    bingo(Calls,Boards,BingoCall,BingoBoards),
+    BingoBoards = [BingoBoard|_],
+    sum_unmarked(BingoBoard,Sum),
+    R is BingoCall * Sum.
+
+solve_2(Calls,Boards,R):-
+    bingo1(Calls,Boards,LastCall,LastBoard),
+    sum_unmarked(LastBoard,Sum),
+    R is LastCall * Sum.
+
+bingo([],_,_,[]).
+bingo([C|Calls], Boards, Call, BingoBoards):-
+    update_boards(Boards, C, NewBoards),
+    check_boards(NewBoards,BBs),
+    (   BBs \= [] 
+        ->  BingoBoards = BBs,
+            Call = C
+        ;   bingo(Calls,NewBoards,Call,BingoBoards)).
+
+bingo1([],_,_,_).
+bingo1([C|Calls], Boards, LastCall, LastBoard):-
+    bingo_round(C,Boards,LC,LB,NewBoards),
+    bingo1(Calls, NewBoards, LC1, LB1),
+    (   bound(LC1) 
+        ->  LastCall = LC1,
+            LastBoard = LB1
+        ;   LastCall = LC,
+            LastBoard = LB).
+
+bingo_round(_,[],_,_,[]).
+bingo_round(C, [Board|Boards], LastCall, LastBoard, NewBoards):-
+    update_board(C,Board,NewBoard),
+    check_board(NewBoard),
+    bingo_round(C, Boards,LC,LB,NewBoards),
+    (   bound(LC) 
+        ->  LastCall = LC,
+            LastBoard = LB
+        ;   LastCall = C,
+            LastBoard = NewBoard).
+bingo_round(C, [Board|Boards], LastCall, LastBoard, [NewBoard|NewBoards]):-
+    update_board(C,Board,NewBoard),
+    \+ check_board(NewBoard),
+    bingo_round(C, Boards, LastCall, LastBoard, NewBoards).
+
+sum_unmarked([],0).
+sum_unmarked([B|Bs],Sum):-
+    sum_unmarked_line(B,S),
+    sum_unmarked(Bs,S1),
+    Sum is S + S1.
+
+sum_unmarked_line([],0).
+sum_unmarked_line([~_|Es],Sum):-
+    sum_unmarked_line(Es,Sum).
+sum_unmarked_line([E|Es],Sum):-
+    \+ marked(E),
+    sum_unmarked_line(Es,S1),
+    Sum is S1 + E.
 
 string_number(S,N):- number_string(N,S).
 
@@ -23,55 +82,42 @@ board_line(Line,Nums):-
     split_string(Line, " ", " ", SNumbs),
     map(string_number, SNumbs, Nums).
 
-boards([],[]). % :- format('multiple_boards: base case~n').
+boards([],[]).
 boards([L|Lines],Boards):-
     board([L|Lines],Board,Rest),
     (   Board = [] -> Boards = Bs;
         Boards = [Board|Bs]),
     boards(Rest, Bs).
 
-board([],[],[]). % :- format('single_board: base case~n').
-board([[]|Ls],[],Ls). % :- format('single_board: rest case~n').
+board([],[],[]).
+board([[]|Ls],[],Ls).
 board([L|Ls],[B|Bs],Rest):-
     board_line(L, B),
     % format('line: ~w~n', [B]),
     board(Ls,Bs,Rest).
 
-print_board([]).
-print_board([L|Ls]):-
-    write(L), nl,
-    print_board(Ls).
+update_boards(Boards,Val,NewBoards):- map(update_board(Val),Boards,NewBoards).
 
-print_boards([]).
-print_boards([B|Bs]):-
-    print_board(B), nl,
-    print_boards(Bs).
+update_board(_,[],[]).
+update_board(Val,[B|Bs],[N|Ns]):-
+    mark(B,Val,N),
+    update_board(Val,Bs,Ns).
 
-get_board_val(B,L,C,V):-
-    nth1(L,B,Line),
-    nth1(C,Line,V).
+mark([],_,[]).
+mark([L|Ls],L,[~L|Ns]):- mark(Ls,L,Ns).
+mark([L|Ls],Val,[L|Ns]):- L \= Val, mark(Ls,Val,Ns).
 
-update_board(Board,Line,Column,Value,NewBoard):-
-    update_board(Bord,1,Line,Column,Value,NewBoard).
-update_board([],_,_,_,_,[]).
-update_board([B|Bs], I, L, C, V, [B|Ns]):-
-    I \= L,
-    I1 is I + 1,
-    update_board(Bs, I1, L, C, V, Ns).
-update_board([B|Bs], I, L, C, V, [N|Ns]):-
-    I = L,
-    update_line(B,C,V,N),
-    I1 is I + 1,
-    update_board(Bs, I1, L, C, V, Ns).
+check_boards(Boards,BingoBoards):- include(check_board,Boards,BingoBoards).
 
-update_line(Line,Column,Value,NewLine):-
-    update_line(Line,1,Column,Value,NewLine).
-update_line([],_,_,_,[]).
-update_line([E|Es],I,C,V,[N|Ns]):-
-    (   I = C -> N = V;
-        N = E),
-    I1 is I + 1,
-    update_line(Es,I1,C,V,Ns).
+check_board(B):-
+    any(check_line,B).
+check_board(B):-
+    transpose(B,B1),
+    any(check_line,B1).
+
+check_line(B):- all(marked,B).
+
+marked(~_).
 
 read_data(File,Lines):-
     open(File, read, Stream),
@@ -85,16 +131,6 @@ read_file(Stream, [X|L]):-
     read_line_to_codes(Stream, X),
     read_file(Stream, L).
 
-prepareLine([],[]).
-prepareLine([X|Xs],[Y|Ys]):-
-    number_codes(Y,[X]),
-    prepareLine(Xs,Ys).
-
-prepare([], []).
-prepare([L|Ls], [X|Xs]):-
-    prepareLine(L,X),
-    prepare(Ls, Xs).
-
 map(_,[],[]).
 map(Callable,[L|Ls],[R|Rs]):-
     call(Callable, L, R),
@@ -105,5 +141,7 @@ reduce(Reducer,[L|Ls],Accu,R):-
     call(Reducer,Accu,L,Accu1),
     reduce(Reducer,Ls,Accu1,R).
 
-solve_1(B,R).
-solve_2(B,R).
+all(P,L):- maplist(P,L).
+any(P,L):- include(P,L,L1), L1 \= [].
+
+bound(V):- nonvar(V).
