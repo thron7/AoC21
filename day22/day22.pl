e669ebc98be513a8d41180d1d4f0330201b8a85c
thron7 <thron7@users.sourceforge.net>
2021-12-26 11:10:04 +0100

    (refac) rename db clauses to `cuboid_state`; cut map/3 making it det

--- day22/day22.pl
+++ day22/day22.pl
@@ -2,17 +2,19 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 :- use_module(library(apply)).
-:- dynamic cuboid/4.
+:- dynamic cuboid_state/4.
 
+% backtracks over all cuboid states and returns corresp. state of given cube
 check_cube_state([X,Y,Z],S):-
-    cuboid(S,x=X1,y=Y1,z=Z1),
+    cuboid_state(S,x=X1,y=Y1,z=Z1),
     X in X1,
     Y in Y1,
     Z in Z1.
 
+% finds the latest state of a cube - is det
 find_cube_state(C,S):-
     findall(S1, check_cube_state(C,S1), States),
-    format('find_cube_state for: ~w~n',[C]),
+    % format('find_cube_state for: ~w~n',[C]),
     (   States = [] 
     ->  S = off
     ;   reverse(States, [S|_])
@@ -27,14 +29,20 @@ cubes_in_cuboid(C,N):-
     format('~:d',[N]),
     N is X * Y * Z.
 
-% count the on cubes from the enumerator
+% count the on cubes from the enumerator - is det.
 count_on_cubes(Enumerator, N):-
     findall(_, (
         call(Enumerator,C),
-        format('backtracking from fcs~n',[]),
+        % format('backtracking from fcs~n',[]),
         find_cube_state(C,on)
         ), OnCubes),
     length(OnCubes,N).
+
+% helper
+cubes_in_hull(N):-
+    hull_cuboid(H),
+    count_on_cubes(enumerate_cuboid(H),N),
+    format('~:d~n',[N]).
 
 % generate the list of cubes in this cuboid
 enumerate_cuboid(C, [X,Y,Z]):-
@@ -44,8 +52,8 @@ enumerate_cuboid(C, [X,Y,Z]):-
     Z in Z1,
     labeling([],[X,Y,Z]).
 
-count_cuboid_clauses(N):-
-    findall(_, cuboid(_,_,_,_), L),
+count_cuboid_states(N):-
+    findall(_, cuboid_state(_,_,_,_), L),
     length(L, N).
     
 
@@ -59,7 +67,7 @@ lines_cuboids([L|Ls]):-
     (   term_range(X,X1),
         term_range(Y,Y1),
         term_range(Z,Z1)
-    ->  assertz(cuboid(S,X,Y,Z))
+    ->  assertz(cuboid_state(S,X,Y,Z))
     ;   true),
     lines_cuboids(Ls).
 
@@ -85,8 +93,8 @@ overlap(XA..XE,YA..YE):-
     YE #>= XA.
 
 test_overlaps():-
-    cuboid(S,X,Y,Z),
-    cuboid(S,X1,Y1,Z1),
+    cuboid_state(S,X,Y,Z),
+    cuboid_state(S,X1,Y1,Z1),
     (   overlap([X,Y,Z],[X1,Y1,Z1])
     ->  format('operlapping: ~w - ~w~n',[[X,Y,Z],[X1,Y1,Z1]])
     ;   true).
@@ -148,14 +156,14 @@ za_lense([_,_,z=ZA.._],ZA).
 ze_lense([_,_,z=_..ZE],ZE).
 
 extrema(State, Lense, MinOrMax, Extremum):-
-    findall([X,Y,Z], cuboid(State,X,Y,Z), L),
+    findall([X,Y,Z], cuboid_state(State,X,Y,Z), L),
     map(Lense,L,L1),
     call(MinOrMax,L1,Extremum).
 
 main(Solution1, Solution2):-
     main('input.txt',Solution1, Solution2).
 main(File, Solution1, Solution2):-
-    read_cuboids(File),
+    read_states(File),
     solve_1(Solution1),
     solve_2(Solution2).
 
@@ -167,8 +175,8 @@ solve_2(S):-
     hull_cuboid(H),
     count_on_cubes(enumerate_cuboid(H), S).
 
-read_cuboids(File):-
-    retractall(cuboid(_,_,_,_)),
+read_states(File):-
+    retractall(cuboid_state(_,_,_,_)),
     read_data(File,Lines),
     lines_cuboids(Lines).
     
@@ -187,5 +195,5 @@ read_file(Stream, [X|L]):-
 map(_,[],[]).
 map(Callable,[L|Ls],[R|Rs]):-
     call(Callable, L, R),
-    map(Callable, Ls, Rs).
+    map(Callable, Ls, Rs), !.
 
